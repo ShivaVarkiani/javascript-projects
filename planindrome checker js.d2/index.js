49eb370a09990092ee95f3b3c5436833517fba98
Shiva Varkiani <164872727+ShivaVarkiani@users.noreply.github.com>
2024-08-23 22:26:14 +0200

    Update index.js

--- planindrome checker js.d2/index.js
+++ planindrome checker js.d2/index.js
@@ -1,6 +1,16 @@
-const input = document.getElementById("input")
+const input = document.getElementById("input");
+
+function reverseString(str) {
+    return str.split("").reverse().join("");
+}
 
 function check() {
-    const value = input.value
-    alert(value)    
+    const value = input.value;
+    const reverse = reverseString(value);
+
+    if (value === reverse) {
+        alert("The word is a palindrome");
+    } else {
+        alert("The word is not a palindrome");
+    }
 }
