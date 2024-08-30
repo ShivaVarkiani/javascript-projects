a556837785a5852e16f5417ca1fa83f1984dab12
Shiva Varkiani <164872727+ShivaVarkiani@users.noreply.github.com>
2024-08-30 22:06:23 +0200

    Update index.js

--- todo list/index.js
+++ todo list/index.js
@@ -1,16 +1,57 @@
 let items = [];
 
-const itemsDiv = document.getElementById("items")
-
-function renderItems(){
-    itemsDiv.innerHtml = null;
-
-    for(const[idx, item] of Object.entries(items)){
-        const text = document.createElement("p")
-        text.textContent=item;
-
-        itemsDiv.appendChild(text)
-    }
+const itemsDiv= document.getElementById("items")
+const input = document.getElementById("itemInput")
+const storageKey = ""
+function loadItems(){
+    const oldItems = localStorage.getItem(storageKey)
+    if (oldItems) items = JSON.parse(oldItems)
+    renderItems()    
 }
 
+function renderItems(){
+    itemsDiv.innerHTML = null;
 
+    for(const[idx,item] of Object.entries(items)) {
+        const container = document.createElement("div")        
+        container.style.marginBottom = "10px"
+
+        const text = document.createElement("p")
+        text.style.display = "inline"
+        text.style.marginRight = "10px"
+        text.textContent = item;
+
+        const button = document.createElement("button")
+        button.textContent = "DELETE"
+        button.onclick = () => removeItem(idx)
+
+        container.appendChild(text)
+        container.appendChild(button)
+
+        itemsDiv.appendChild(container)
+    }
+
+}
+function saveItems(){
+    const stringItem= JSON.stringify(items) ;
+    localStorage.setItem(storageKey, stringItem)
+}
+
+function addItem(){
+    const value = input.value
+    if(!value){
+        alert("no value")
+        return
+    } 
+    items.push(value)
+    renderItems()
+    input.value =""
+    saveItems()
+}
+
+function removeItem (idx){
+    items.splice(idx,1)
+    renderItems()
+    saveItems()
+}
+document.addEventListener("DOMContentLoaded",loadItems)
